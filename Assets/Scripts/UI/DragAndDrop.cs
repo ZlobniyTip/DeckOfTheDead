b29d7648279a02ada9abec59998cd9cb8a1edc38
vfrfv <123299470+vfrfv@users.noreply.github.com>
2024-11-17 16:35:15 +0300

    Сorrecting drag and drop

--- Assets/Scripts/UI/DragAndDrop.cs
+++ Assets/Scripts/UI/DragAndDrop.cs
@@ -4,9 +4,12 @@ using UnityEngine.EventSystems;
 public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     [SerializeField] private GameObject _dragObject;
+    [SerializeField] private GameObject _cardView;
 
     private RectTransform _rectTransform;
     private Vector3 _originalPosition;
+
+    private GameObject _gameObject;
 
     private void Awake()
     {
@@ -16,32 +19,57 @@ public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, ID
     public void OnBeginDrag(PointerEventData eventData)
     {
         _originalPosition = transform.position;
+
+        if (FindSpawnLocation(out Vector3 spawnPosition))
+        {
+            _cardView.gameObject.SetActive(false);
+            _gameObject = Instantiate(_dragObject, spawnPosition, Quaternion.identity);
+            _gameObject.transform.position = spawnPosition;
+        }
+        else
+        {
+            _gameObject = null;
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         _rectTransform.anchoredPosition += eventData.delta;
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        if (FindSpawnLocation(out Vector3 spawnPosition))
         {
-            _dragObject.transform.position = hit.point;
+            _gameObject.transform.position = spawnPosition;
         }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        if (_gameObject != null && FindSpawnLocation(out Vector3 spawnPosition))
+        {
+            _gameObject.transform.position = spawnPosition;
+        }
+        else
+        {
+            if (_gameObject != null)
+            {
+                Destroy(_gameObject);
+            }
+            transform.position = _originalPosition;
+            _cardView.gameObject.SetActive(true);
+        }
+    }
+
+    private bool FindSpawnLocation(out Vector3 spawnPosition)
+    {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            Instantiate(_dragObject, hit.point, Quaternion.identity);
-            Destroy(gameObject);
+            spawnPosition = hit.point;
+            return true;
         }
-        else
-        {
-            transform.position = _originalPosition;
-        }
+
+        spawnPosition = _originalPosition;
+        return false; 
     }
 }
