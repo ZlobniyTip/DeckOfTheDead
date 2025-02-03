e11740f9615dd876398067eb455d5d87fbc27718
ZlobniyTip <4opick_97@mail.ru>
2025-02-03 18:09:56 +0300

    Money counter

--- Assets/Scripts/UI/MoneyCounter.cs
+++ Assets/Scripts/UI/MoneyCounter.cs
@@ -6,7 +6,7 @@ public class MoneyCounter : MonoBehaviour
     [SerializeField] private Buyer _buyer;
     [SerializeField] private TMP_Text _money;
 
-    private void Start()
+    private void Awake()
     {
         _buyer.MoneyChanged += ChangeMoney;
     }
